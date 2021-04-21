0d3c5dcdb46a75533aeb66c0acac803af57f943c
pguyal <pguyal@hotmail.com>
2021-04-21 15:41:21 -0400

    Create index curl script for questions

---
+++ curl-scripts/questions/index.sh
@@ -0,0 +1,11 @@
+# TOKEN=<user token> sh curl-scripts/questions/index.sh
+
+API="http://localhost:4741"
+URL_PATH="/questions"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request GET \
+  --header "Authorization: Bearer ${TOKEN}"
+
+echo
